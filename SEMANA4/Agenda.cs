372ebd35baa93fd01f94fd76a37434d783c45bab
AlexPaillacho <ad.natop@uea.edu.ec>
2025-06-25 22:18:18 -0500

    arreglos

---
+++ SEMANA4/Agenda.cs
@@ -0,0 +1,20 @@
+﻿﻿
+Console.WriteLine("Universidad Estatal Amazónica");
+Console.WriteLine("=============================");
+Console.WriteLine();
+
+RegistroContactos registro = new RegistroContactos(20);
+
+Contacto uno = new Contacto("Alex", 0965321020);
+Contacto dos = new Contacto("Lucas", 0986523140);
+Contacto tres = new Contacto("Lorena", 0965321478);
+
+registro.IngresoNuevocontacto(uno, 0);
+registro.IngresoNuevocontacto(dos, 1);
+registro.IngresoNuevocontacto(tres, 2);
+
+registro.imprimirnombres();
+
+Console.WriteLine("Ingrese el nombre del contacto a buscar");
+registro.buscar(Console.ReadLine());
+
---
+++ SEMANA4/clsRegistroContactos.cs
@@ -0,0 +1,88 @@
+public class RegistroContactos
+{
+    public Contacto[] _nombres { get; set; }
+
+   
+       public RegistroContactos(int Maximonombres)
+    {
+        _nombres = new Contacto[Maximonombres];
+    }
+
+    public void IngresoNuevocontacto(Contacto _numero, int posicion)
+    {
+        _nombres[posicion] = _numero;
+    }
+
+    public void imprimirnombres()
+    {
+        Console.WriteLine("Listado de contactos registradas");
+        System.Console.WriteLine("===========================");
+
+        for (int i = 0; i < _nombres.Length; i++)
+        {
+            //while (continuar == true)
+            //{
+            if (_nombres[i] != null)
+            {
+                Console.WriteLine("nombre " + _nombres[i].Nombre);
+            }
+            //     else
+            //     {
+            //         continuar = false;
+            //     }
+            // }
+        }
+    }
+
+
+    public void buscar(string nombrecontacto)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Búsqueda de contacto");
+        System.Console.WriteLine("===========================");
+        if (encontrar(nombrecontacto))
+        {
+            Console.WriteLine("El registro fue encontrado");
+        }
+        else
+        {
+            Console.WriteLine("No se encuentro el registro");
+        }
+    }
+
+
+   
+
+
+    private bool encontrar(string nombre)
+    {
+        bool encontrado = false;
+
+        for (int i = 0; i < _nombres.Length; i++)
+        {
+            if (_nombres[i] != null)
+            {
+                if (nombre == _nombres[i].Nombre)
+                {
+                    encontrado = true;
+                }
+            }
+        }
+
+        return encontrado;
+    }
+}
+
+public class Contacto
+{
+    public string Nombre { get; set; }
+    public double Telefono { get; set; }
+
+    // Constructor
+    public Contacto(string nombre, int _numero)
+    {
+        Nombre = nombre;
+        Telefono = _numero;
+
+    }
+}

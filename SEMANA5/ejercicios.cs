a8a00e0aa072855995c518ff11c5be10cce46a21
AlexPaillacho <ad.natop@uea.edu.ec>
2025-06-29 21:08:28 -0500

    cambios

---
+++ SEMANA5/ejercicios.cs
@@ -0,0 +1,15 @@
+
+Console.WriteLine("=============================");
+
+Console.WriteLine("UNIVERSIDAD ESTATAL AMAZONICA");
+
+Console.WriteLine("==============================");
+
+List<string> asignaturas = ["Ingles", "Metodologia", "Sistemas", "Fisica"];
+
+foreach (string item in asignaturas)
+
+
+Console.WriteLine(item);
+
+

01167183ac55532415b5a4e72d5b84e236f9f313
AlexPaillacho <ad.natop@uea.edu.ec>
2025-07-12 22:06:36 -0500

    cambios

--- SEMANA7/Formula Balanceada.cs
+++ SEMANA7/Formula Balanceada.cs
@@ -1,69 +1,69 @@
 ﻿
-// // clase revisar expresiones
-// public class REVISAREXPRESIONES
-// {
-//     public static void Main(string[] args)
-//     {
-//         Console.WriteLine("   Verificación de Paréntesis Balanceados  ");
-//         Console.WriteLine("Ingrese una expresión matemática:");
-//         string expression = Console.ReadLine();
+// clase revisar expresiones
+public class REVISAREXPRESIONES
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("   Verificación de Paréntesis Balanceados  ");
+        Console.WriteLine("Ingrese una expresión matemática:");
+        string expression = Console.ReadLine();
 
-//         if (IsBalanced (expression))
-//         {
-//             Console.WriteLine("Salida: Fórmula balanceada");
-//         }
-//         else
-//         {
-//             Console.WriteLine("Salida: Fórmula No balanceada");
-//         }
+        if (IsBalanced (expression))
+        {
+            Console.WriteLine("Salida: Fórmula balanceada");
+        }
+        else
+        {
+            Console.WriteLine("Salida: Fórmula No balanceada");
+        }
 
-//         Console.WriteLine("\nPresione cualquier tecla para salir  ");
-//     }
+        Console.WriteLine("\nPresione cualquier tecla para salir  ");
+    }
 
-//     // Verificamos si la funcion esta correcta o balanceada
-//     public static bool IsBalanced(string expression)
-//     {
+    // Verificamos si la funcion esta correcta o balanceada
+    public static bool IsBalanced(string expression)
+    {
 
-//         Stack<char> stack = new Stack<char>();
-
-
-//         foreach (char c in expression)
-//         {
-
-//             if (c == '(' || c == '{' || c == '[')
-//             {
-//                 stack.Push(c);
-//             }
-
-//             else if (c == ')' || c == '}' || c == ']')
-//             {
-
-//                 if (stack.Count == 0)
-//                 {
-//                     return false;
-//                 }
+        Stack<char> stack = new Stack<char>();
 
 
-//                 char topChar = stack.Pop();
+        foreach (char c in expression)
+        {
+
+            if (c == '(' || c == '{' || c == '[')
+            {
+                stack.Push(c);
+            }
+
+            else if (c == ')' || c == '}' || c == ']')
+            {
+
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
 
 
-//                 switch (c)
-//                 {
-//                     case ')':
-//                         if (topChar != '(') return false;
-//                         break;
-//                     case '}':
-//                         if (topChar != '{') return false;
-//                         break;
-//                     case ']':
-//                         if (topChar != '[') return false;
-//                         break;
-//                 }
-//             }
-
-//         }
+                char topChar = stack.Pop();
 
 
-//         return stack.Count == 0;
-//     }
-// }
+                switch (c)
+                {
+                    case ')':
+                        if (topChar != '(') return false;
+                        break;
+                    case '}':
+                        if (topChar != '{') return false;
+                        break;
+                    case ']':
+                        if (topChar != '[') return false;
+                        break;
+                }
+            }
+
+        }
+
+
+        return stack.Count == 0;
+    }
+}
